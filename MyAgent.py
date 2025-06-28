071d59d947fca5b91047156fe2ea1e4b7696ac12
venurao983 <106814802+venurao983@users.noreply.github.com>
2025-06-28 11:34:45 -0500

    Update MyAgent.py

--- MyAgent.py
+++ MyAgent.py
@@ -1,20 +1,34 @@
-import random
 from Game2048 import *
 
 class Player(BasePlayer):
-	def __init__(self, timeLimit):
-		BasePlayer.__init__(self, timeLimit)
+    def __init__(self, timeLimit):
+        BasePlayer.__init__(self, timeLimit)
 
-	def findMove(self, board):
-		bestScore = -1000
-		bestMove = ''
-		
-		for a in board.actions():
-			print('Testing', a)
-			m = board.move(a)
-			if m.getScore() > bestScore:
-				bestScore = m.getScore()
-				bestMove = a
-				
-		self.setMove(bestMove)
-		
+    def findMove(self, board):
+        moves = board.actions()
+        bestMove = moves[0]
+        bestScore = -1
+
+        for move in moves:
+            nextState = board.move(move)
+            score = self.expectimax(nextState, 2)
+            if score > bestScore:
+                bestScore = score
+                bestMove = move
+
+        self.setMove(bestMove)
+
+    def expectimax(self, board, depth):
+        if depth == 0 or board.gameOver():
+            return board.getScore()
+
+        moves = board.actions()
+        total = 0
+        for move in moves:
+            nextState = board.move(move)
+            total += self.expectimax(nextState, depth -1)
+
+        if moves:
+            return total / len(moves)
+        else:
+            return 0
